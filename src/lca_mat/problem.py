04a72c67abc7ff7cbcb15aa66555baee0bf35d36
kiunana <isuru_nanayakkara@live.com>
2024-08-05 13:37:44 -0700

    some code implementations added

--- src/lca_mat/problem.py
+++ src/lca_mat/problem.py
@@ -1,10 +1,14 @@
-
+from .processess import ProcessessMatrix
 
 __author__ = ["POD/LCA Team"]
 __copyright__ = "Univrsity of Washington"
 __license__ = "MIT License"
 __email__ = "kiun@uw.edu"
 __version__ = "0.1.0"
+
+# =================================
+# PROBLEM CLASS
+# =================================
 
 class Problem(object):
 
@@ -13,6 +17,10 @@ class Problem(object):
         self.demand = {}
         self.process_matrix = None
         self.problem_settings = None
+
+    # =================================
+    # Setters / Getters
+    # =================================
 
     def set_database(self, database):
         """ Sets database for the problem.
@@ -24,7 +32,16 @@ class Problem(object):
         """
         self.database = database
 
-    def create_process_matrix():
+    def get_database(self):
+        """ Gets database for the problem.
+        """
+        return self.database
+
+    # =================================
+    # Methods
+    # =================================
+
+    def create_process_matrix(self):
         """ Creates the full process matrix.
         
         Inputs:
@@ -36,7 +53,16 @@ class Problem(object):
         
         """
         
-        pass
+        P = ProcessessMatrix()
+        inventory_vector = P.get_basis()
+        database = self.get_database()
+        unit_processes = database.get_unit_processess()
+        for unit_process in unit_processes:
+            exchanges = unit_process.get_exchanges()
+            for exchange in exchanges:
+                inventory_vector.add_inventory_item(exchange)
+                ## this only give the exchange name... how to transfer the other details of the exchange
+            P.add_process()
 
 
     def solve():
