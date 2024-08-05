adca507f4ee869801947bd1dbd102afbb275daed
kiunana <isuru_nanayakkara@live.com>
2024-08-05 15:25:20 -0700

    Further implementations of methods. Needs testing.

--- src/lca_mat/LCI_database.py
+++ src/lca_mat/LCI_database.py
@@ -5,6 +5,10 @@ __license__ = "MIT License"
 __email__ = "kiun@uw.edu"
 __version__ = "0.1.0"
 
+# =================================
+# LCI DATABASE CLASS
+# =================================
+
 class LCIDatabase():
     
     def __init__(self):
@@ -12,25 +16,46 @@ class LCIDatabase():
         self.name = None
         self.data = None
 
-    def import_data():
-
-        pass
-
-    def update_data():
-
-        pass
-
     def get_unit_processess():
+        """  Returns a list of UnitProcess Objects.
+        
+        """
 
         # TODO: implement method
         pass
+
+# =================================
+# UNIT PROCESS CLASS
+# =================================
 
 class UnitProcess():
 
     def __init__(self):
         self.exchanges = {}
+        # TODO: Check other properties needed to be stored
 
-    def get_exchanges():
+    def get_exchanges(self):
 
-        pass
+        return self.exchanges
     
+    def set_exchange(self, name, exchange):
+
+        if not (exchange in self.get_exchanges.keys()):
+            self.exchanges[name] = exchange
+        else:
+            raise NotImplementedError
+
+# =================================
+# EXCHANGE CLASS
+# =================================
+
+class Exchange():
+
+    def __init__(self):
+        self.name = None
+        self.qty = 0.0
+        self.unit = None
+
+    def get_qty(self):
+
+        return self.qty
--- src/lca_mat/processess.py
+++ src/lca_mat/processess.py
@@ -11,13 +11,21 @@ __version__ = "0.1.0"
 class ProcessessMatrix():
 
     def __init__(self):
-        self.P = []
+        self.mat_data = {}
         self.basis = {}
 
     # =================================
     # Setters / Getters
     # =================================
 
+    def get_mat_data(self):
+        """ Gets process matrix data.
+            Matrix is kept as a dictionary of dictionaries, where the outer dictionary key is column number (process) 
+            and the inner dictionary key is the row numbern (inventory item) and the value is the value (qty) on the 
+            corresponding entry of the matrix.
+        """
+        return self.mat_data
+    
     def get_basis(self):
         """ Gets basis (i.e., inventory vector) of the processes space.
         """
@@ -27,20 +35,25 @@ class ProcessessMatrix():
     # Methods
     # =================================
 
-    def add_process():
+    def add_process(self, exchanges):
         """ Adds a process to the Processes Matrix (P).
         
         Inputs:
         ------
-        
-        
-        Returns:
-        -------
+            exchanges : dict.
+                Dictionary of all exchanges.
         
         """
-        
-        # TODO: write implementation
-        pass
+        mat_data = self.get_mat_data()
+        col = len(mat_data)
+
+        inventory = self.get_basis()
+        new_process = {}
+        for exchange in exchanges.keys():
+            row = inventory[exchange]
+            qty = exchanges[exchange].get_qty()
+            new_process[row] = qty
+        mat_data[col] = new_process
 
 # =================================
 # INVENTORY VECCTOR CLASS
@@ -64,7 +77,7 @@ class InventoryVector():
     # Methods
     # =================================
 
-    def add_inventory_item(self, name, row, place=None, unit=None, qty=None):
+    def add_inventory_item(self, name, properties={}):
         """ Adds a new item to the inventory (q).
         
         Inputs:
@@ -77,9 +90,10 @@ class InventoryVector():
         """
         
         inventory = self.get_inventory()
+        n = len(inventory)
         if not (name in inventory.keys()):
-            new_inventory_item = InventoryItem(name, row)
-            # TODO: Set other properties of the inventory item
+            new_inventory_item = InventoryItem(name, n)
+            new_inventory_item.set_properties(properties)
             inventory[name] = new_inventory_item
 
 # =================================
@@ -94,3 +108,7 @@ class InventoryItem():
         self.place = None
         self.unit = None
         self.qty = 0.0
+
+    def set_properties(self, properties={}):
+        
+        pass
