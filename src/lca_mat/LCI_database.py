dee415cd238526ea6f23bae509cd1df9e5f7959e
kiunana <isuru_nanayakkara@live.com>
2024-08-05 11:07:07 -0700

    Initial code structure created. No implementations.

---
+++ src/lca_mat/LCI_database.py
@@ -0,0 +1,25 @@
+
+__author__ = ["POD/LCA Team"]
+__copyright__ = "Univrsity of Washington"
+__license__ = "MIT License"
+__email__ = "kiun@uw.edu"
+__version__ = "0.1.0"
+
+class LCIDatabase():
+    
+    def __init__(self):
+
+        self.name = None
+        self.data = None
+
+    def import_data():
+
+        pass
+
+    def update_data():
+
+        pass
+
+    def get_unit_process():
+
+        pass
---
+++ src/lca_mat/__init__.py
@@ -0,0 +1,11 @@
+
+__author__ = ["POD/LCA Team"]
+__copyright__ = "Univrsity of Washington"
+__license__ = "MIT License"
+__email__ = "kiun@uw.edu"
+__version__ = "0.1.0"
+
+from .LCI_database import *
+from .problem import *
+from .processess import *
+from .linear_algebra import *
