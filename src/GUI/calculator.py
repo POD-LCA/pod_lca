36dd339b6a0c036b49984d845243e8bf2409e2e1
kiunana <isuru_nanayakkara@live.com>
2024-08-13 12:49:07 -0700

    GUI updates - calculator class and other objects (processes and flows) bound to GUI - tooltips and context menus to objects added - connectors improved

---
+++ src/GUI/calculator.py
@@ -0,0 +1,12 @@
+class Calculator():
+
+    def __init__(self, processes, flows, exchanges):
+
+        self.processes = processes
+        self.flows = flows
+        self.exchanges = exchanges
+
+    def calculate(self):
+
+        return 2
+        
