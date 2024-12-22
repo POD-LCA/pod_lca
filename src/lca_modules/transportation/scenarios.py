8a6ff44782694907ae0595d074b3cec160369b34
Mhtaba76 <mtaba76@gmail.com>
2024-12-21 18:23:17 -0800

    impacts for all scenarios bug fixed

--- src/lca_modules/transportation/scenarios.py
+++ src/lca_modules/transportation/scenarios.py
@@ -57,22 +57,24 @@ class Scenario:
         self.na = None
         self.global_ = None
 
-        self.pre_processing()
+        self.pre_us_processing()
+        self.pre_global_processing()
 
-    def pre_processing (self):
+    def pre_us_processing(self):
+
 
         data_material = self.project.get_subdataset("EC3 Category to CFS Group mapping")
         cfs = self.project.get_subdataset("cfs_2017")
         emission = self.project.get_subdataset("Emission")
-        cfs= cfs.drop(columns=["SHIPMT_ID","QUARTER","SHIPMT_VALUE","TEMP_CNTL_YN","EXPORT_CNTRY","HAZMAT","WGT_FACTOR", "ORIG_MA","ORIG_CFS_AREA","DEST_MA","DEST_CFS_AREA"])
+        cfs = cfs.drop(columns=["SHIPMT_ID", "QUARTER", "SHIPMT_VALUE", "TEMP_CNTL_YN", "EXPORT_CNTRY",
+                                "HAZMAT", "WGT_FACTOR", "ORIG_MA", "ORIG_CFS_AREA", "DEST_MA", "DEST_CFS_AREA"])
 
-        sctg = data_material[data_material["material"] == self.material].iloc[0,1]
+        sctg = data_material[data_material["material"] == self.material].iloc[0, 1]
         sctg = int(str(sctg)[:2])
-        df = cfs[cfs ["SCTG"] == str(sctg)].copy()
+        df = cfs[cfs["SCTG"] == str(sctg)].copy()
         quartiles = df["SHIPMT_DIST_ROUTED"].quantile([0.25, 0.5, 0.75]).values
 
         def assign_quartile(x, q1, q2, q3):
-
             if x <= q1:
                 return 'Q1'
             elif x <= q2:
@@ -82,65 +84,49 @@ class Scenario:
             else:
                 return 'Q4'
 
-        df.loc[:, 'quartile'] = df["SHIPMT_DIST_ROUTED"].apply(assign_quartile, args=(quartiles[0], quartiles[1], quartiles[2]))
+        df['quartile'] = df["SHIPMT_DIST_ROUTED"].apply(assign_quartile, args=(quartiles[0], quartiles[1], quartiles[2]))
 
-        local = df[df['quartile'] == 'Q1']
-        regional = df[df['quartile'] == 'Q2']
-        regional_c = df[df['quartile'] == 'Q3']
-        national = df[df['quartile'] == 'Q4']
+        quartile_mapping = {
+            "Local": df[df['quartile'] == 'Q1'],
+            "Regional": df[df['quartile'] == 'Q2'],
+            "Regional_c": df[df['quartile'] == 'Q3'],
+            "National": df[df['quartile'] == 'Q4']
+        }
 
-        if self.mode == None:
+        def process_scenario(df, emission, mode=None):
+            df = pd.merge(emission, df, on="MODE")
+            if mode:
+                df = df[df["mode_name"] == mode]
+            impact_cols = df.columns[4:9]
+            df[impact_cols] = df[impact_cols].multiply(df["SHIPMT_DIST_ROUTED"], axis=0)
+            return df[impact_cols].mean().to_dict()
 
-            if self.scenario == "Local":
-                local = pd.merge (emission, local, on ="MODE")
-                local.iloc[:,4:9] = local.iloc[:,4:9].multiply(local['SHIPMT_DIST_ROUTED'], axis=0)
-                self.local = local.iloc[:,4:9].mean().to_dict()
+        # Process all scenarios dynamically
+        for scenario_name, scenario_df in quartile_mapping.items():
+            impact = process_scenario(scenario_df, emission, mode=self.mode)
+            setattr(self, scenario_name.lower(), impact)
 
-            elif self.scenario == "Regional":
-                regional = pd.merge (emission, regional, on ="MODE")
-                regional.iloc[:,4:9] = regional.iloc[:,4:9].multiply(regional['SHIPMT_DIST_ROUTED'], axis=0)
-                self.regional = regional.iloc[:,4:9].mean().to_dict()
+    
 
-            elif self.scenario == "Regional_c":
-                regional_c = pd.merge (emission, regional_c, on ="MODE")
-                regional_c.iloc[:,4:9] = regional_c.iloc[:,4:9].multiply(regional_c['SHIPMT_DIST_ROUTED'], axis=0)
-                self.regional_c = regional_c.iloc[:,4:9].mean().to_dict()
+    def pre_global_processing (self):
 
-            elif self.scenario == "National":
-                national = pd.merge (emission, national, on ="MODE")
-                national.iloc[:,4:9] = national.iloc[:,4:9].multiply(national['SHIPMT_DIST_ROUTED'], axis=0)
-                self.national = national.iloc[:,4:9].mean().to_dict()
+        data_material = self.project.get_subdataset("EC3 Category to CFS Group mapping")
+        emission = self.project.get_subdataset("Emission")
+        faf = pd.read_csv (r"temp\transportation_dataset\FAF561.csv")
+        PODlLCA_file_path = self.project.get_subdataset("PODlLDA_transport_dataset")
+        dist_fr = PODlLCA_file_path.parse(sheet_name="MOT")
+        sensitive_material = PODlLCA_file_path.parse(sheet_name="sensitive_material")
+        faf_dist_band = PODlLCA_file_path.parse(sheet_name="faf_dist_band")
+        cfaf = self.project.get_subdataset ("CFAF_C2011-2017_Code_E")
 
-            else:
-                print ("error")
 
-        else:
 
-            if self.scenario == "Local":
-                local = pd.merge (emission, local, on ="MODE")
-                local = local[local ["mode_name"] == self.mode]
-                local.iloc[:,4:9] = local.iloc[:,4:9].multiply(local['SHIPMT_DIST_ROUTED'], axis=0)
-                self.local = local.iloc[:,4:9].mean().to_dict()
 
-            elif self.scenario == "Regional":
-                regional = pd.merge (emission, regional, on ="MODE")
-                regional = regional[regional ["mode_name"] == self.mode]
-                regional.iloc[:,4:9] = regional.iloc[:,4:9].multiply(regional['SHIPMT_DIST_ROUTED'], axis=0)
-                self.regional = regional.iloc[:,4:9].mean().to_dict()
+        sctg = data_material[data_material["material"] == self.material].iloc[0,1]
+        sctg = int(str(sctg)[:2])
 
-            elif self.scenario == "Regional_c":
-                regional_c = pd.merge (emission, regional_c, on ="MODE")
-                regional_c = regional_c[regional_c ["mode_name"] == self.mode]
-                regional_c.iloc[:,4:9] = regional_c.iloc[:,4:9].multiply(regional_c['SHIPMT_DIST_ROUTED'], axis=0)
-                self.regional_c = regional_c.iloc[:,4:9].mean().to_dict()
 
-            elif self.scenario == "National":
-                national = pd.merge (emission, national, on ="MODE")
-                national = national[national ["mode_name"] == self.mode]
-                national.iloc[:,4:9] = national.iloc[:,4:9].multiply(national['SHIPMT_DIST_ROUTED'], axis=0)
-                self.national = national.iloc[:,4:9].mean().to_dict()
 
-        
     def scenario_impact (self):
 
         if self.scenario == "Local":
@@ -154,6 +140,9 @@ class Scenario:
 
         if self.scenario == "National":
             return self.national
+
+        if self.scenario == "Global":
+            return self.global_
 
     def get_local_impact (self):
 
@@ -171,6 +160,10 @@ class Scenario:
 
         return self.national
 
+    def get_global_impact (self):
+
+        return self.global_
+
 
 
 if __name__ == '__main__':
@@ -179,5 +172,8 @@ if __name__ == '__main__':
 
     data_folder = r"C:\Users\mhtaba\Desktop\pod_lca_git\pod_lca\data\transportation_dataset"
     project = ProjectLogisticManager(name="Building A", location="Seattle", data_folder=data_folder)
-    project.create_link ( material="Carpet", qty=200, travel_dist="Local", return_trip_factor=1.5, dist_unit="km", mode= None, eff=0.9)
-    print (project.get_impact())
+    #project.create_link ( material="Carpet", qty=1, travel_dist="Global", return_trip_factor=1.5, dist_unit="km", mode= "Truck", eff=0.9)
+    
+    scenario = Scenario(project, "Local", "Carpet", "Truck")
+    print (scenario.get_regional_impact())
+    #print (project.get_impact())
