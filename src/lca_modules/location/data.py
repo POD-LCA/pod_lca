614ecdd5d2fb382ef225b8fae278fc130282f106
Isuru Nanayakkara <isuru_nanayakkara@live.com>
2025-01-22 15:12:52 -0800

    #113 review of the location module

---
+++ src/lca_modules/location/data.py
@@ -0,0 +1,10 @@
+CFS_DATA_PATH = "data\\location_cfs.csv"
+
+FAF_DATA = {"Canada": 801, 
+            "Mexico": 802, 
+            "Rest of Americas": 803,
+            "Europe": 804, 
+            "Africa": 805, 
+            "SW & Central Asia": 806,
+            "Eastern Asia": 807, 
+            "SE Asia & Oceania": 808}
