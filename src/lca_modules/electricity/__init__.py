d9acb9f724e8b9c9954b3028ed815c2eb6ffd5d2
Isuru Nanayakkara <isuru_nanayakkara@live.com>
2025-04-21 12:11:02 -0700

    #111 data files renamed

--- src/lca_modules/electricity/__init__.py
+++ src/lca_modules/electricity/__init__.py
@@ -8,13 +8,13 @@ DEFAULT_YEAR = 2025
 DEFAULT_SCENARIO = 'MidCase'
 DEFAULT_DECLARED_UNIT = MEGA * WATT_HOUR
 
-CAMBIUM_NATIONAL_DATA = "data\\cambium_data_national.csv"
-CAMBIUM_REGIONAL_DATA = "data\\cambium_data_regional.csv"
-CAMBIUM_LOCAL_DATA = "data\\cambium_data_local.csv"
+CAMBIUM_NATIONAL_DATA = "data\\electricity_cambium_consumption-national.csv"
+CAMBIUM_REGIONAL_DATA = "data\\electricity_cambium_consumption-regional.csv"
+CAMBIUM_LOCAL_DATA = "data\\electricity_cambium_consumption-local.csv"
 CAMBIUM_DATA_YEARS = [2025, 2030, 2035, 2040, 2045, 2050]
-CAMBIUM_HEADER_MAP = "data\\cambium_headers.json"
-CAMBIUM_TECHNOLOGY_MAP = "data\\cambium_technology_map.json"
-CAMBIUM_REGIONS_MAP = "data\\cambium_regions_map.json"
+CAMBIUM_HEADER_MAP = "data\\electricity_cambium_technology-headers.json"
+CAMBIUM_TECHNOLOGY_MAP = "data\\electricity_cambium_technology-map.json"
+CAMBIUM_REGIONS_MAP = "data\\electricity_cambium_regions-map.json"
 
 # File path to the National level electricity data to be provided here
 # The data file should have the following columns
@@ -24,7 +24,7 @@ CAMBIUM_REGIONS_MAP = "data\\cambium_regions_map.json"
 #   <impact categories> : Impact category names should match the impact categories used across the project (default path: data/impact_categories.json.
 #                           Impact category names are case sensitive.
 
-ELECTRICITY_IMPACT_NATIONAL_DATA = "data\\National_consumption_impacts_by_technology.csv"
+ELECTRICITY_IMPACT_NATIONAL_DATA = "data\\electricity_netl_national-consumption-impacts-by-technology.csv"
 
 # File path to the Regional level electricity data to be provided here
 # The data file should have the following columns
@@ -33,6 +33,6 @@ ELECTRICITY_IMPACT_NATIONAL_DATA = "data\\National_consumption_impacts_by_techno
 #   <impact categories> : Impact category names should match the impact categories used across the project (default path: data/impact_categories.json.
 #                           Impact category names are case sensitive.
 
-ELECTRICITY_IMPACT_REGIONAL_DATA = "data\\Regional_consumption_impacts_by_technology.csv"
+ELECTRICITY_IMPACT_REGIONAL_DATA = "data\\electricity_netl_regional-consumption-impacts-by-technology.csv"
 
-ELECTRICITY_TECHNOLOGIES = "data\\NETL_electricity_technologies.csv"
+ELECTRICITY_TECHNOLOGIES = "data\\electricity_netl_electricity-technologies.csv"
--- src/lca_modules/location/data.py
+++ src/lca_modules/location/data.py
@@ -1,8 +1,8 @@
 CFS_DATA_PATH = "data\\location_cfs.csv"
-FERC_BA_MAP_PATH = "data\\FERC_BA_mapping.csv"
-BA_ZIPCODE_MAP_PATH = "data\\BA_zip_mapping.csv"
-GEA_ZIPCODE_MAP_PATH = "data\\GEA_REEDS_zip_mapping.csv"
-REEDS_BA_ZIPCODE_MAP_PATH = "data\\GEA_REEDS_zip_mapping.csv"
+FERC_BA_MAP_PATH = "data\\location_netl_ferc-ba-mapping.csv"
+BA_ZIPCODE_MAP_PATH = "data\\location_doe_ba-zip-mapping.csv"
+GEA_ZIPCODE_MAP_PATH = "data\\location_cambium_gea-reeds-zip-mapping.csv"
+REEDS_BA_ZIPCODE_MAP_PATH = "data\\location_cambium_gea-reeds-zip-mapping.csv"
 
 FAF_DATA = {"Canada": 801, 
             "Mexico": 802, 
