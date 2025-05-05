2f582db53b5e1a2f33d1d673c4f4cdf58d60ca26
Isuru Nanayakkara <isuru_nanayakkara@live.com>
2025-05-05 15:11:05 -0700

    #79 units map docstring

--- src/utilities/units/units_map.py
+++ src/utilities/units/units_map.py
@@ -1,6 +1,16 @@
 from utilities.units.common_units import KILOGRAM, JOULE, KILOMETER, WATT_HOUR, TON_KILOMETER, CUBIC_METER, LITER, HOUR, SQUARE_METER, ITEM, CUBIC_FEET, US_GALLON
 from utilities.units.metric_prefixes import KILO, MEGA
 
+__author__ = ["POD/LCA Team"]
+__copyright__ = "University of Washington"
+__license__ = "MIT License"
+__email__ = "kiun@uw.edu"
+__version__ = "0.1.0"
+
+
+# This file contains a mapping of strings to their corresponding unit objects.
+# This is to be used for conversions strings in import files (CSV, JSON, etc.) to their corresponding unit objects in the code.
+# The mapping is not exhaustive and can be extended as needed.
 
 UNITS_MAP = {
                 'h': HOUR,
