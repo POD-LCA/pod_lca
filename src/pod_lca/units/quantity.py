420843cd03ff738c0e0675b35e8e2a340dd5dc65
Tomas Mendez Echenagucia <tmendeze@uw.edu>
2026-01-16 12:02:20 -0800

    minor quantity upodate

--- src/pod_lca/units/quantity.py
+++ src/pod_lca/units/quantity.py
@@ -12,15 +12,22 @@ class Quantity(object):
 
     def __str__(self):
         return '{} ({})'.format(self.value, self.unit.name)
-    
-    def __mul__(self, val):
-        f = val.unit.convert_to(self.unit)
-        return Quantity(self.value * (val.value * f), self.unit)
 
     def __add__(self, val):
         f = val.unit.convert_to(self.unit)
         return Quantity(self.value + (val.value * f), self.unit)
-    
+
+    def __sub__(self, val):
+        f = val.unit.convert_to(self.unit)
+        return Quantity(self.value - (val.value * f), self.unit)
+
+    def __mul__(self, val):
+        f = val.unit.convert_to(self.unit)
+        return Quantity(self.value * (val.value * f), self.unit)
+
+    def __truediv__(self, val):
+        f = val.unit.convert_to(self.unit)
+        return Quantity(self.value / (val.value * f), self.unit)
 
 
 if __name__ == '__main__':
