d34547f8cc7aa49be7e65a87f45df593b61b1358
Ethan Ellingboe <etel5501@uw.edu>
2026-06-05 18:09:16 -0600

    Fix handling of empty SCTG Category in ProductTransportationMixins
    Updated the logic to return "N/A" when the SCTG Category is empty instead of returning a default SCTG code. This change improves the clarity of the output when no mapping is found for the given NAICS Sub-category.

--- src/pod_lca/lca_modules/materials_screening/product_transportation_mixins.py
+++ src/pod_lca/lca_modules/materials_screening/product_transportation_mixins.py
@@ -195,8 +195,8 @@ class ProductTransportationMixins:
                 naics_sub_cat = data["NAICS Sub-category"]
                 mapped = mapping[mapping["NAICS Sub-category"] == naics_sub_cat].iloc[0:1]
 
-                if mapped["SCTG Category"].iloc[0] is None:
-                    return config['setup']['transportation']['DEFAULT_SCTG_CODE']
+                if mapped["SCTG Category"].empty:
+                    return "N/A"
                 mapped_sctg = mapped["SCTG Category"].iloc[0]
                 if isinstance(mapped_sctg, (int, float)):
                     if isnan(mapped_sctg):
